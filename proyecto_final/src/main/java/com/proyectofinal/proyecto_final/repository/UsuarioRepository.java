87ca371c99cefc80698842dba166933fc836e6a8
julian-jaramillo97 <115040097+julian-jaramillo97@users.noreply.github.com>
2024-06-18 14:39:17 -0500

    Delete proyecto_final directory

--- proyecto_final/src/main/java/com/proyectofinal/proyecto_final/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyectofinal.proyecto_final.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.proyectofinal.proyecto_final.domain.*;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
-}
